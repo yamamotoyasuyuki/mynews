190ca6f748f07952788c65402b94e5d483a6cdf1
yamamotoyasuyuki <yasuyuki_yamamoto.23@icloud.com>
2022-01-25 02:24:17 +0000

    situmon

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
 // Route::get('XXX/XXX', 'AAAController＠bbb');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
